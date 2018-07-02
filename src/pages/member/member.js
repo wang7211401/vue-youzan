064016c8c7bbfa510a998ad0be10730f590f8a62
wang7211401 <wang7211401@163.com>
2018-07-02 09:01:04 +0800

    地址管理

--- src/pages/member/member.js
+++ src/pages/member/member.js
@@ -1,2 +1,31 @@
-import './member_base.css'
-import './member.css'
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+let routes =[{
+    path:'/',
+    component:require('./components/member.vue')
+},{
+    path:'/address',
+    component:require('./components/address.vue'),
+    children:[{
+        path:'',
+        component:require('./components/all.vue')
+    },{
+        path:'all',
+        component:require('./components/all.vue')
+    },{
+        path:'form',
+        component:require('./components/form.vue')
+    }]
+}]
+
+let router = new Router({
+    routes
+})
+
+new Vue({
+    el:'#app',
+    router
+})
